e11078959ecc35a74aa8085de9f368e737056bb0
Caitto <souzacaitto@gmail.com>
2023-09-01 10:35:24 -0300

    att

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,16 +1,27 @@
-import styles from "./menu.module.css"
+import styles from "./menu.module.css";
 
-const Menu = () => {
-  return(
-    <div className={styles.menuBand}>
-<ul>
-  <li><a href="#">Inicio</a></li>
-  <li><a href="#">Séries</a></li>
-  <li><a href="#">Filmes</a></li>
-  <li><a href="#">Catálogo</a></li>
-</ul>
-    </div>
-  )
-}
+const Menu = ({ click }) => {
+  return (
+    <>
+      <div className={styles.fundoMenu}  onClick={click}></div>
+      <div className={styles.menuBand}>
+        <ul>
+          <li>
+            <a href="#">Início</a>
+          </li>
+          <li>
+            <a href="#">Séries</a>
+          </li>
+          <li>
+            <a href="#">Filmes</a>
+          </li>
+          <li>
+            <a href="#">Catálogo</a>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
 
 export default Menu;
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,32 +1,31 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
 import logo from "../assets/devflix.png";
 import searchIcon from "../assets/search.svg";
-import "https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js";
-import { useState } from "react";
 
+import "https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js";
 import "./App.css";
 import MovieCard from "../components/movieCard/movieCard";
 import Footer from "../components/footer/footer";
-import Menu from "../components/menu/Menu";
+import Menu from "../components/menu/menu";
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [movies, setMovies] = useState([]);
-  const [isMenu, setMenu] = useState(true);
-  const apiKey = "e4d577fa";
+  const [isMenu, setIsMenu] = useState(false);
 
+  const apiKey = "e4d577fa";
   const apiUrl = `https://omdbapi.com/?apikey=${apiKey}`;
-  
 
   useEffect(() => {
-    searchMovies("Avengers");
+    searchMovies("Batman");
   }, []);
 
   const searchMovies = async (title) => {
     const response = await fetch(`${apiUrl}&s=${title}`);
     const data = await response.json();
 
-    console.log(data);
+    // console.log(data);
     setMovies(data.Search);
   };
 
@@ -34,38 +33,37 @@ const App = () => {
     e.key === "Enter" && searchMovies(searchTerm);
   };
 
-const toggMenu = () => {
-  setIsMenu(!isMenu);
-}
-
+  const toggleMenu = () => {
+    setIsMenu(!isMenu);
+    console.log(isMenu);
+  };
   return (
     <div id="app">
-      <div className="app">
-        <img src={logo} alt="logo devflix"></img>
+      <div className="logo">
+        <img src={logo} alt="logo devflix" />
       </div>
-      
+
       <div className="searchBar">
-      <ion-icon name="reorder-four-outline" onClick=(toggMenu) />
-        <Menu cli />
-      </div>
-      
-      <div className="search">
-        <input
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          onKeyDown={handleKeyPress}
-          placeholder="pesquise aqui "
-        />
-        <img
-          src={searchIcon}
-          alt="Icone de pesquisa"
-          onClick={() => searchMovies(searchTerm)}
-        />
+        <ion-icon name="menu" onClick={toggleMenu} />
+        {isMenu && <Menu click={toggleMenu} />}
+        <div className="search">
+          <input
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            onKeyDown={handleKeyPress}
+            placeholder="Pesquise por filmes"
+          />
+          <img
+            src={searchIcon}
+            alt="Icone de pesquisa"
+            onClick={() => searchMovies(searchTerm)}
+          />
+        </div>
       </div>
       {movies?.length > 0 ? (
         <div className="container">
           {movies.map((movie) => (
-            <MovieCard key ={movie.imdbID}  movies={movie}/>
+            <MovieCard key={movie.imdbID} movies={movie} />
           ))}
         </div>
       ) : (
@@ -73,13 +71,9 @@ const toggMenu = () => {
           <h2>Nenhum filme encontrado 😢</h2>
         </div>
       )}
-      <Footer link={"https:github.com.br"}>Caitto</Footer>
+      <Footer link={"https:github.com.br"}>ProfCastello</Footer>
     </div>
   );
 };
 
-
-
-
 export default App;
-
